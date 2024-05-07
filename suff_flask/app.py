05b207db3a05966fc0c49a97a6d63fe004006639
codeNBogus <4840sss@kookmin.ac.kr>
2024-05-07 22:01:07 +0900

    조회요건 추가

--- suff_flask/app.py
+++ suff_flask/app.py
@@ -9,18 +9,25 @@ app = Flask(__name__)
 def index():
     #get을 통해 전달받은 데이터를 확인
     key1 = request.args.get('keyword1')
-    key2= request.args.get('keyword2')
-    print(key1, key2)
+    key2 = request.args.get('keyword2')
+    key3 = request.args.get('keyword3')
+    key4 = request.args.get('keyword4')
+    key5 = request.args.get('keyword5')
 
-    if key1 is None or key2 is None:
+    print(key1, key2, key3, key4, key5)
+
+    if key1 is None or key2 is None or key3 is None or key4 is None or key5 is None:
         return render_template('index.html')
     else:
         #키워드 크롤링
         value1 = google_keywoed.get_keyword_number(key1)
         value2 = google_keywoed.get_keyword_number(key2)
+        value3 = google_keywoed.get_keyword_number(key3)
+        value4 = google_keywoed.get_keyword_number(key4)
+        value5 = google_keywoed.get_keyword_number(key5)
 
         # feedback data
-        data = {'key1:': value1, 'key2' : value2}
+        data = {'key1:': value1, 'key2' : value2, 'key3' : value3, 'key4' : value4, 'key5' : value5}
         return render_template('index.html', data=data)
 
 #메인 테스트
